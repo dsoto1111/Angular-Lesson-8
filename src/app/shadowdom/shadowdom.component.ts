d2f5a92edd5255ffd8b2882e84c6ed9b3ea92ffb
dsoto1111 <130852526+dsoto1111@users.noreply.github.com>
2023-05-23 13:40:36 -0400

    1 file added

---
+++ src/app/shadowdom/shadowdom.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-shadowdom',
+  templateUrl: './shadowdom.component.html',
+  styleUrls: ['./shadowdom.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom,
+})
+export class ShadowdomComponent {}
